0b534bae36743380b70a60c689438153d0853c8a
Tullia Glaeser <tamglaeser@users.noreply.github>
2023-12-04 20:00:22 +0100

    Configured testing DB, implemented 1 integration test and 2 unit tests in the back, verifying the processing/importing of a CSV file

---
+++ tests/Feature/ProcessCSVFileTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class ProcessCSVFileTest extends TestCase
+{
+    use RefreshDatabase; // This trait resets the database after each test
+    /** @test */
+    public function it_processes_csv_file_from_url() {
+        $csvContent = <<<CSV
+            Reviews Content,Rating,Start Date,Address,Appartments,Source
+            "The app hardly ever works. I have restarted more than 10 times, even reinstalled and still can't view the menu 😏",1,2021-08-17 00:00:00,"801 Silbury Blvd, Milton Keynes MK9 3FL, United Kingdom",Solstice Apartments,Google
+            "Very bad housing association. They want your money and that’s all. They don’t care about problems or defects. They allowed to build houses with windows that let the draught passing through and it’s a waste of energy bills. The traffic noise is horrible as well. Very bad experience
+
+            ",1,2021/09/18,"Ferry Ln, London N17 9NF, United Kingdom",Windlass Apartments,Truspilot
+            CSV;
+        Http::fake([
+            'http://feedier-production.s3.eu-west-1.amazonaws.com/*' => Http::response($csvContent, 200),
+        ]);
+
+        $this->artisan('csv:process')
+            ->expectsOutput('Feedier CSV file processed successfully')
+            ->assertExitCode(0);
+
+        $this->assertDatabaseHas('feedback', [
+            'review' => "The app hardly ever works. I have restarted more than 10 times, even reinstalled and still can't view the menu 😏",
+            'rating' => 1,
+            'start_date' => "2021-08-17 00:00:00",
+            'address' => "801 Silbury Blvd, Milton Keynes MK9 3FL, United Kingdom",
+            'appartments' => "Solstice Apartments",
+            'source' => "Google"
+        ]);
+
+        $this->assertDatabaseHas('feedback', [
+            'review' => "Very bad housing association. They want your money and that’s all. They don’t care about problems or defects. They allowed to build houses with windows that let the draught passing through and it’s a waste of energy bills. The traffic noise is horrible as well. Very bad experience",
+            'rating' => 1,
+            'start_date' => "2021/09/18",
+            'address' => "Ferry Ln, London N17 9NF, United Kingdom",
+            'appartments' => "Windlass Apartments",
+            'source' => "Truspilot"
+        ]);
+    }
+}
---
+++ tests/Unit/FileProcessingServiceTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Tests\Unit;
+use App\Models\Feedback;
+use App\Services\FileProcessingService;
+use Exception;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class FileProcessingServiceTest extends TestCase
+{
+    use RefreshDatabase; // This trait resets the database after each test
+
+    /** @test */
+    public function it_processes_csv_content_correctly()
+    {
+        $fileService = new FileProcessingService();
+
+        // Mock CSV content for testing
+        $csvContent = "Reviews Content,Rating,Start Date,Address,Appartments,Source\nReview 1,5,2023-01-01,Address 1,Solstice Apartments,Google\nReview 2,4,2023-01-02,Address 2,Kings Dock Mill Liverpool,Truspilot";
+
+        // Call the method to process CSV content
+        $fileService->processCSVContent($csvContent);
+
+        $this->assertDatabaseCount('feedback', 2);
+        $this->assertDatabaseHas('feedback', [
+            'review' => "Review 1",
+            'rating' => 5,
+            'start_date' => "2023-01-01",
+            'address' => "Address 1",
+            'appartments' => "Solstice Apartments",
+            'source' => "Google"
+        ]);
+        $this->assertDatabaseHas('feedback', [
+            'review' => "Review 2",
+            'rating' => 4,
+            'start_date' => "2023-01-02",
+            'address' => "Address 2",
+            'appartments' => "Kings Dock Mill Liverpool",
+            'source' => "Truspilot"
+        ]);
+    }
+
+    /** @test */
+    public function it_throws_exception_on_invalid_rating()
+    {
+        $fileService = new FileProcessingService();
+
+        // Mock CSV content with an invalid rating
+        $csvContent = "review,rating,start_date,address,appartments,source\nInvalid Review,0,2023-01-01,Address 1,3,Source 1";
+
+        // Use try-catch to capture the exception thrown by the method
+        try {
+            $fileService->processCSVContent($csvContent);
+
+            // If an exception is not thrown, fail the test
+            $this->fail('Expected Exception was not thrown.');
+        } catch (Exception $e) {
+            // Assert that the exception message contains the expected string
+            $this->assertStringContainsString('Validation failed', $e->getMessage());
+        }
+
+        // Ensure that no feedback entries were created due to the invalid rating
+        $this->assertEquals(0, Feedback::count());
+    }
+}
